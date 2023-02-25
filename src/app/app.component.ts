b8962d45c38f6c610ac17aad989aaab69de790c1
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-02-25 18:03:28 +0200

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyApp';
+  title = 'my-app';
+  hero = 'Drogfisher';
+
+  onLike() {
+    window.alert(`I like ${this.hero}`);
+    this.hero = 'Boothstomper';
+  }
 }
